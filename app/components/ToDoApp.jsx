47d13d87361ca26ffef8f967378156c1a3e2ba59
Lily Nguyen <lnguyen1110@gmail.com>
2017-06-22 16:52:43 -0700

    Added Search todos and updated multiple todos

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -1,10 +1,13 @@
 const React = require('react');
-var ToDoList = require('ToDoList');
-
+const ToDoList = require('ToDoList');
+const AddTodo = require('AddToDo');
+const ToDoSearch = require('ToDoSearch');
 
 const ToDoApp = React.createClass({
 	getInitialState: function() {
 		return {
+			showCompleted: false,
+			searchTodos: '',
 			todos: [
 				{
 					id: 1,
@@ -22,14 +25,33 @@ const ToDoApp = React.createClass({
 			]
 		};
 	},
-  render: function() {
-  	let {todos} = this.state;
-  	return(
-  		<div>
-  			<ToDoList todos={todos}/>
-  		</div>
-  	)
-  }
+  	handleSubmit: function (text) {
+  		var todosArray = this.state.todos;
+	  	var idArray = [];
+  		for (var i =0; i < todosArray.length; i++) {
+  			var id = todosArray[i].id;
+  			idArray.push(id);
+  		}
+  		var newId = idArray.length+1;
+  		todosArray.push({id: newId, text: text});
+  		this.setState(todosArray);
+  	},
+  	handleSearch: function(showCompleted, searchTodos) {
+  		this.setState({
+  			showCompleted: showCompleted,
+  			searchTodos: searchTodos.toLowerCase()
+  		});
+  	},
+	render: function() {
+		let {todos} = this.state;
+			return(
+				<div>
+					<ToDoSearch onSearch={this.handleSearch}/>
+					<ToDoList todos={todos}/>
+			        <AddTodo onAddTodo={this.handleSubmit}/>
+				</div>
+			)
+	}
 });
 
 module.exports = ToDoApp;
---
+++ app/components/ToDoSearch.jsx
@@ -0,0 +1,26 @@
+const React = require('react');
+
+const ToDoSearch = React.createClass({
+	onHandleSearch: function() {
+		let showCompleted = this.refs.showCompleted.checked;
+		let searchTodos = this.refs.searchTodos.value;
+		
+		this.props.onSearch(showCompleted, searchTodos);
+	},
+	render: function () {
+		return (
+			<div>
+				<div>
+					<input type="search" ref="searchToDos" placeholder="Search ToDos" onChange={this.handleSearch}/>
+				</div>
+				<div>
+					<label>
+						<input type="checkbox" ref="showCompleted" placeholder="Search ToDos" onChange={this.handleSearch}/>
+					</label>
+				</div>
+			</div>
+		);
+	}
+});
+
+module.exports = ToDoSearch;
